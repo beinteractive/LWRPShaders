464bd853b90b26d1f16bd1ff9d6ffb16d86a811f
Yoshihiro Shindo <beinteractive.org@gmail.com>
2018-04-17 11:33:25 +0900

    Update for LWRP 1.1.4-preview

--- Shaders/ShaderLibrary - DepthOnlyPass.hlsl
+++ Shaders/ShaderLibrary - DepthOnlyPass.hlsl
@@ -1,6 +1,12 @@
 #ifndef LWRPSHADRES_SHADERLIBRARY_DEPTH_ONLY_PASS
 #define LWRPSHADRES_SHADERLIBRARY_DEPTH_ONLY_PASS
 
+#ifndef USE_BUILTIN_ALPHA_CLIP
+
+#define Alpha(x, y, z) ;
+
+#endif
+
 #include "LWRP/ShaderLibrary/LightweightPassDepthOnly.hlsl"
 
 #endif
--- Shaders/ShaderLibrary - ShadowPass.hlsl
+++ Shaders/ShaderLibrary - ShadowPass.hlsl
@@ -1,6 +1,12 @@
 #ifndef LWRPSHADRES_SHADERLIBRARY_SHADOW_PASS
 #define LWRPSHADRES_SHADERLIBRARY_SHADOW_PASS
 
+#ifndef USE_BUILTIN_ALPHA_CLIP
+
+#define Alpha(x, y, z) ;
+
+#endif
+
 #include "LWRP/ShaderLibrary/LightweightPassShadow.hlsl"
 
 #endif
