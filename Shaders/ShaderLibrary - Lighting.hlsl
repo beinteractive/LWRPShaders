a0f8968c541d1d4bce803b0d332e3bd1f50e4e89
Yoshihiro Shindo <beinteractive.org@gmail.com>
2019-10-18 18:49:20 +0900

    ComputeShadowCoord function

--- Shaders/ShaderLibrary - Lighting.hlsl
+++ Shaders/ShaderLibrary - Lighting.hlsl
@@ -3,4 +3,13 @@
 
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
 
+float4 ComputeShadowCoord(float3 ws, float4 cs)
+{
+#if SHADOWS_SCREEN
+    return ComputeScreenPos(cs);
+#else
+    return TransformWorldToShadowCoord(ws);
+#endif
+}
+
 #endif
